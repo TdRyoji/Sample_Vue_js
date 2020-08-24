7b923eb6d54eb06612d67313bb364ff7f31332cc
Tdrj2716 <ri76joy@gmail.com>
2020-08-24 16:31:13 +0900

    learn v-for

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 
-var app = new Vue({
-  el: '#app',
+var app1 = new Vue({
+  el: '#app1',
   data: {
     message1: 'Hello Vue.js!',
     message2: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    list: [
+      { text: 'Learn Javascript' },
+      { text: 'Learn Vue' },
+      { text: 'Read 3 books a week'}
+    ]
   }
 })
